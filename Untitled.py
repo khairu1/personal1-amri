7f87db6649e662c997cfb478a608ad8e35d1b69a
khairu1 <128364732+khairu1@users.noreply.github.com>
2023-06-14 14:04:10 +0700

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print('hello world')
+print('saya sedang belajar dengan phyton')
+print('menggunakan anakConda')
+print('menggunakan anakConda')
+print('selamat belajar gess❤👌')
+
+
+# In[6]:
+
+
+def greeting('bangkit'):
+    return 'hello, ' + 'bangkit'
+print (greeting('bangkit'))
+
+
+# In[7]:
+
+
+print(1<10)
+
+
+# In[8]:
+
+
+print("Linux" == "windows")
+
+
+# In[10]:
+
+
+def check(str):
+    if str > 0:
+        return "positive"
+    elif str == 0:
+        return "zero"
+    else:
+        return "negative"
+
+
+# In[13]:
+
+
+while x > 0:
+    print("positive x=" + str(x))
+    x = x - 1
+
+print("now x=" + str(x))
+
+
+# In[14]:
+
+
+for x in range(3):
+    print("x=" + str(x))
+
+
+# In[15]:
+
+
+###break
+
+for X in range(3):
+    print("x=" + str(x))
+    if x == 1:
+        break
+
+
+# In[17]:
+
+
+###continue
+
+for X in range(3, 0, -1):
+    if x % 2 == 0:
+        continue
+    print(x)
+
+
+# In[ ]:
+
+
+
+
